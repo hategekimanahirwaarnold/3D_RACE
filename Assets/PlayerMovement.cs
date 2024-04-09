badcea12669d346897157f160ba87db29477fc3d
Hirwa Arnold <macbook@macbooks-MacBook-Pro.local>
2024-04-09 21:17:57 +0200

    Getting started

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
     }
 }
+
