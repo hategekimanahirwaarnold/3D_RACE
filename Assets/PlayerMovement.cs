19df13440e9a347f2d913807c15b1d5880f5cc41
Hirwa Arnold <macbook@macbooks-MacBook-Pro.local>
2024-04-10 01:00:08 +0200

    Level I

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -12,22 +12,22 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey("s"))
         {
-            rb.AddForce(0, 0, -sidewaysForce * Time.deltaTime);
+            rb.AddForce(0, 0, -sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("w"))
         {
-            rb.AddForce(0, 0, sidewaysForce * Time.deltaTime);
+            rb.AddForce(0, 0, sidewaysForce * Time.deltaTime, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         if ( Input.GetKey("a"))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
     }
